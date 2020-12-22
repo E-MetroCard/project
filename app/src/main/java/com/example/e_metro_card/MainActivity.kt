508b18bac848066ae489fe79f5187198ed413293
Pranav Gupta <guptapranav4299@gmail.com>
2020-12-22 23:06:07 +0530

    added routes activity and intents

--- app/src/main/java/com/example/e_metro_card/MainActivity.kt
+++ app/src/main/java/com/example/e_metro_card/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.e_metro_card
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapbtn.setOnClickListener{
+            val mapInt=Intent(this, dmrc_map::class.java)
+            startActivity(mapInt)
+        }
+
+
+        loginbtn.setOnClickListener{
+            val loginint=Intent(this, loginscreen::class.java)
+            startActivity(loginint)
+        }
+
+        routesbtn.setOnClickListener{
+            val routesInt=Intent(this, routesStations::class.java)
+            startActivity(routesInt)
+        }
     }
 }
