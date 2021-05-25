7ae86c63b65b67b88acf91b2380c793d9be1ba97
Pranav Gupta <guptapranav4299@gmail.com>
2021-05-25 23:12:46 +0530

    resolved some constraint attributes on the index screen

--- app/src/main/java/com/example/e_metro_card/qr.kt
+++ app/src/main/java/com/example/e_metro_card/qr.kt
@@ -68,13 +68,13 @@ class qr : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         if(::codeScanner.isInitialized) {
-            codeScanner?.startPreview()
+            codeScanner.startPreview()
         }
     }
 
     override fun onPause() {
         if(::codeScanner.isInitialized) {
-            codeScanner?.releaseResources()
+            codeScanner.releaseResources()
         }
         super.onPause()
     }
